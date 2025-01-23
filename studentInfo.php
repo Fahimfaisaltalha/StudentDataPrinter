793984c15d3985a699ec59136c315154a3a55606
Fahimfaisaltalha <faisal15-4721@diu.edu.bd>
2025-01-24 01:56:01 +0600

    First Student Info

--- studentInfo.php
+++ studentInfo.php
@@ -1,4 +1,12 @@
 <?php
-
+$allStudentsInfo=[
+    ["Name"=>"Fahim Faisal Talha", "ID"=> 1,  "Age"=>24, "CGPA"=>"3.82"]
+];
+foreach($allStudentsInfo as $studentsInfo){
+    foreach($studentsInfo as $key=>$value){
+        echo "$key : $value"."\t";
+    }
+    echo "\n";
+}
 
 ?>
